5c7a1e5bce37fe66f89d67023f45cfffb851c5d2
Aiste Laugayte <aiste.laugalyte@gmail.com>
2021-04-25 18:42:57 +0300

    Added login test with invalid credentials and test for one product page

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,14 +2,41 @@ const puppeteer = require('puppeteer');
 const assert = require('chai').assert;
 
 describe("Login Testing", () => {
+    let browser;
+    let page;
+
+    before("Launch browser and go to login page", async function() {
+        browser = await puppeteer.launch({headless: false});
+        page = await browser.newPage();
+        await page.goto('https://www.saucedemo.com/');
+    });
+
+    it("Login as Standard User with invalid credentials", async () => {
+        await page.type('#user-name', 'standard_user1');
+        await page.type('#password', 'secret');
+        await page.click('#login-button');
+        const errorMessage = await page.$eval('[data-test="error"]', errorMsg => errorMsg.textContent);
+        assert.equal(errorMessage, "Epic sadface: Username and password do not match any user in this service");
+    });
+
     it("Login as Standard User with valid credentials", async () => {
-      const browser = await puppeteer.launch({ headless: false });
-      const page = await browser.newPage();
-      await page.goto('https://www.saucedemo.com/');
-      await page.type('#user-name', 'standard_user');
-      await page.type('#password', 'secret_sauce');
-      await page.click('#login-button');
-      assert.include(page.url(), "inventory.html");
-      await browser.close();
-    })
-})
+        const usernameField = await page.$('#user-name');
+        await usernameField.click({clickCount: 3});
+        // await page.keyboard.press('Backspace');
+        await usernameField.type('standard_user')
+        const passwordField = await page.$('#password');
+        await passwordField.click({clickCount: 3});
+        // await page.keyboard.press('Backspace');
+        await passwordField.type('secret_sauce');
+        await page.click('#login-button');
+        assert.include(page.url(), "inventory.html");
+        await page.waitForSelector('span.title');
+        const title = await page.$eval('span.title', text => text.textContent);
+        assert.equal(title, "Products");
+    });
+
+    after("Close browser", async function() {
+        await browser.close();
+    });
+
+});
---
+++ tests/productPage.test.js
@@ -0,0 +1,48 @@
+const puppeteer = require('puppeteer');
+const assert = require('chai').assert;
+
+describe("One product page", () => {
+    let browser;
+    let page;
+
+    before("Launch browser and login as standard user", async function() {
+        browser = await puppeteer.launch({headless: false});
+        page = await browser.newPage();
+        await page.goto('https://www.saucedemo.com/');
+        await page.type('#user-name', 'standard_user');
+        await page.type('#password', 'secret_sauce');
+        await page.click('#login-button');
+    });
+
+    it("Check if product page has required information", async () => {
+        await page.waitForSelector('#item_4_img_link');
+        await page.click('#item_4_img_link');
+        assert.include(page.url(), "?id=4");
+        await page.waitForSelector('.inventory_details_desc_container');
+        const title = await page.$eval('.inventory_details_name', text => text.textContent);
+        assert.equal(title, "Sauce Labs Backpack");
+        let description = await page.$('.inventory_details_desc');
+        assert.exists(description);
+        description = await page.$eval('.inventory_details_desc', text => text.textContent);
+        assert.isNotEmpty(description);
+        const price = await page.$eval('.inventory_details_price', price => price.textContent);
+        assert.match(price, /^\$\d+\.\d{2}$/);
+        const addBtn = await page.$eval('#add-to-cart-sauce-labs-backpack', btn => btn.textContent);
+        assert.equal(addBtn, "Add to cart");
+        const img = await page.$('img.inventory_details_img');
+        assert.exists(img);
+    });
+
+    it("Go back to all products", async () => {
+        await page.click('#back-to-products');
+        assert.include(page.url(), "inventory.html");
+        await page.waitForSelector('span.title');
+        const title = await page.$eval('span.title', text => text.textContent);
+        assert.equal(title, "Products");
+    });
+
+    after("Close browser", async function() {
+        await browser.close();
+    });
+
+});
